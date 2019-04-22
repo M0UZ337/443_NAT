d8546c4342b66462cab7147b36b5ba05fc37da3f
M0UZ337 <thomas980712@gmail.com>
2019-04-23 00:19:44 +0800

    Updated iptable.h and iptable.c

--- iptable.c
+++ iptable.c
@@ -7,3 +7,70 @@
 //
 
 #include "iptable.h"
+
+IPtable* makeIPtable()
+{
+    IPtable *iptable = (IPtable *)malloc(sizeof(IPtable));
+    iptable->head = iptable->tail = NULL;
+    return iptable;
+}
+
+Entry* makeEntry(Address *original_address, Address *translated_address)
+{
+    Entry *entry = (Entry *)malloc(sizeof(Entry));
+    entry->next = NULL;
+    entry->original_address = original_address;
+    entry->translated_address = translated_address;
+    return entry;
+}
+
+void printTable(IPtable *iptable)
+{
+    //...
+    return;
+}
+
+void newEntry(Entry *entry, IPtable *iptable)
+{
+    if (iptable->head == NULL)
+    {
+        iptable->head = entry;
+        iptable->tail = entry;
+    }
+    else
+    {
+        iptable->tail->next = entry;
+        iptable->tail = entry;
+    }
+}
+
+int deleteEntry(Address *original_address, IPtable *iptable)
+{
+    int state = 0;
+    Entry *prev = (Entry *)malloc(sizeof(Entry));
+    Entry *search = (Entry *)malloc(sizeof(Entry));
+    prev = NULL;
+    search = iptable->head;
+    while (search != NULL)
+    {
+        if (search->original_address == original_address)
+        {
+            //Head is the target
+            if (prev == NULL)
+            {
+                iptable->head = iptable->head->next;
+                state = 1;
+                break;
+            }
+            else
+            {
+                prev->next = search->next;
+                state = 1;
+                break;
+            }
+        }
+        prev = search;
+        search = search->next;
+    }
+    return state;
+}
--- iptable.h
+++ iptable.h
@@ -10,5 +10,49 @@
 #define iptable_h
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <netinet/in.h>
+
+
+//Structure List
+
+typedef struct Address Address;
+typedef struct Entry Entry;
+typedef struct IPtable IPtable;
+
+struct Address
+{
+    uint32_t ip;
+    uint16_t port;
+};
+
+struct Entry
+{
+    Entry *next;
+    Address *original_address;
+    Address *translated_address;
+};
+
+struct IPtable
+{
+    Entry *head;
+    Entry *tail;
+};
+
+
+//Function List
+
+//Make IPtable
+IPtable* makeIPtable();
+//Make Entry
+Entry* makeEntry(Address *original_address, Address *translated_address);
+//Print the iptable, from head to tail
+void printTable(IPtable *iptable);
+//Translate the Address first and pass into the function
+void newEntry(Entry *entry, IPtable *iptable);
+//Delete the Entry by searching it using original address (return 0 if fail/ 1 if success)
+int deleteEntry(Address *original_address, IPtable *iptable);
 
 #endif /* iptable_h */
