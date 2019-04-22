33006dbcef1208f8d377f5d069132cf31e176527
70NY-L1 <47357967+70NY-L1@users.noreply.github.com>
2019-04-23 00:21:14 +0800

    nfqueue

--- NAT.c
+++ NAT.c
@@ -1,8 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h> // required by "netfilter.h"
+#include <arpa/inet.h> // required by ntoh[s|l]()
+#include <signal.h> // required by SIGINT
+#include <string.h> // required by strerror()
+#include <sys/time.h> // required by gettimeofday()
+#include <time.h> // required by nanosleep()
+#include <errno.h> // required by errno
+#include <pthread.h>
+#include <netinet/ip.h>        // required by "struct iph"
+#include <netinet/tcp.h>    // required by "struct tcph"
+#include <netinet/udp.h>    // required by "struct udph"
+#include <netinet/ip_icmp.h>    // required by "struct icmphdr"
+
 #include "NAT.h"
+
+#include "checksum.h"
+static int Callback(struct nfq_q_handle *myQueue, struct nfgenmsg *msg,
+                    struct nfq_data *pkt, void *cbData) {
+}
 
 int main(int argc, const char * argv[])
 {
-    printf("Hello World !!!");
+    // Get a queue connection handle from the module
+    struct nfq_handle *nfqHandle;
+    if (!(nfqHandle = nfq_open())) {
+        fprintf(stderr, "Error in nfq_open()\n");
+        exit(-1);
+    }
+    
+    // Unbind the handler from processing any IP packets
+    if (nfq_unbind_pf(nfqHandle, AF_INET) < 0) {
+        fprintf(stderr, "Error in nfq_unbind_pf()\n");
+        exit(1);
+    }
+    
+    // Install a callback on queue 0
+    struct nfq_q_handle *nfQueue;
+    if (!(nfQueue = nfq_create_queue(nfqHandle,  0, &Callback, NULL))) {
+        fprintf(stderr, "Error in nfq_create_queue()\n");
+        exit(1);
+    }
+    // nfq_set_mode: I want the entire packet
+    if(nfq_set_mode(nfQueue, NFQNL_COPY_PACKET, BUF_SIZE) < 0) {
+        fprintf(stderr, "Error in nfq_set_mode()\n");
+        exit(1);
+    }
+    
+    struct nfnl_handle *netlinkHandle;
+    netlinkHandle = nfq_nfnlh(nfqHandle);
+    
+    int fd;
+    fd = nfnl_fd(netlinkHandle);
+    
+    int res;
+    char buf[BUF_SIZE];
+    
+    while ((res = recv(fd, buf, sizeof(buf), 0)) && res >= 0) {
+        nfq_handle_packet(nfqHandle, buf, res);
+    }
+    
+    nfq_destroy_queue(nfQueue);
+    nfq_close(nfqHandle);
     return 0;
 }
