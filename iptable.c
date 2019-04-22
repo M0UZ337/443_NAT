bcf78959bdd7fa84b708b521659104a5d029ba28
M0UZ337 <thomas980712@gmail.com>
2019-04-22 23:24:57 +0800

    Added iptable.h and iptable.c

---
+++ iptable.c
@@ -0,0 +1,9 @@
+//
+//  iptable.c
+//  443_NAT
+//
+//  Created by Mo Mo on 22/4/2019.
+//  Copyright © 2019 Mo Mo. All rights reserved.
+//
+
+#include "iptable.h"
---
+++ iptable.h
@@ -0,0 +1,14 @@
+//
+//  iptable.h
+//  443_NAT
+//
+//  Created by Mo Mo on 22/4/2019.
+//  Copyright © 2019 Mo Mo. All rights reserved.
+//
+
+#ifndef iptable_h
+#define iptable_h
+
+#include <stdio.h>
+
+#endif /* iptable_h */
